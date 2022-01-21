c42fbe5176234437a75a50fc62eda6179706e9af
Josh Herrmann <jdvh1292@gmail.com>
2022-01-20 22:03:51 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go-pi-tempsensor
+
+go 1.17
+
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stianeikeland/go-rpio/v4 v4.6.0
+	periph.io/x/conn/v3 v3.6.10
+	periph.io/x/devices/v3 v3.6.13
+	periph.io/x/host/v3 v3.7.2
+)
+
+require golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stianeikeland/go-rpio/v4"
+	"periph.io/x/conn/v3/i2c/i2creg"
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/devices/v3/bmxx80"
+	"periph.io/x/host/v3"
+)
+
+func getPinState(i int) rpio.State {
+	pin := rpio.Pin(i)
+	curState := pin.Read()
+	fmt.Printf("Pin %v curState: %v\n", i, curState)
+	return curState
+}
+
+func setPinState(i int, targetState int) {
+	pin := rpio.Pin(i)
+	pin.Output()
+}
+
+func getAllPinState() {
+	count := 27
+	for i := 0; i < count; i++ {
+		getPinState(i)
+	}
+}
+
+func flipPinLoop() {
+	pinList := []int{17, 18, 27, 22, 23, 24, 25, 4, 5, 6, 13, 19, 26, 12, 16, 20, 21}
+	for _, pin := range pinList {
+		fmt.Printf("PIN:%v\n", pin)
+		pinObj := rpio.Pin(pin)
+		pinObj.Output()
+		pinObj.Toggle()
+		time.Sleep(time.Second)
+		pinObj.Toggle()
+	}
+}
+
+func bme280() {
+	if _, err := host.Init(); err != nil {
+		panic(err)
+	}
+
+	bus, err := i2creg.Open("")
+	if err != nil {
+		panic(err)
+	}
+	defer bus.Close()
+
+	dev, err := bmxx80.NewI2C(bus, 0x76, &bmxx80.DefaultOpts)
+	if err != nil {
+		panic(err)
+	}
+	defer dev.Halt()
+
+	var env physic.Env
+	if err = dev.Sense(&env); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%8s %10s %9s\n", env.Temperature, env.Pressure, env.Humidity)
+}
+
+func forever() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	log.Info("Shutdown signal received...")
+}
+
+func stats_Loop() {
+	var interval time.Duration = 20
+	for {
+		//getAllPinState()
+		//bme280()
+		flipPinLoop()
+		time.Sleep(interval * time.Second)
+	}
+}
+
+func main() {
+	err := rpio.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer rpio.Close()
+
+	go stats_Loop()
+
+	forever()
+}
