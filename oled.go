5e1eb33a90e6d3d9115b7a6853608be5b1cc5cf7
Joshua Herrmann <jdvh1292@gmail.com>
2022-01-30 13:11:41 -0500

    Swap back to GIF

--- oled.go
+++ oled.go
@@ -3,15 +3,14 @@ package main
 import (
 	"image"
 	"image/draw"
+	"image/gif"
 	"log"
+	"os"
+	"time"
 
 	"github.com/nfnt/resize"
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/basicfont"
-	"golang.org/x/image/math/fixed"
 	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/devices/v3/ssd1306"
-	"periph.io/x/devices/v3/ssd1306/image1bit"
 	"periph.io/x/host/v3"
 )
 
@@ -54,47 +53,48 @@ func oled(envChan chan EnvData) {
 	}
 	defer oledDev.Halt()
 
+	// Decodes an animated GIF as specified on the command line:
+	if len(os.Args) != 2 {
+		log.Fatal("please provide the path to an animated GIF")
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	g, err := gif.DecodeAll(f)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Converts every frame to image.Gray and resize them:
+	imgs := make([]*image.Gray, len(g.Image))
+	for i := range g.Image {
+		imgs[i] = convertAndResizeAndCenter(oledDev.Bounds().Dx(), oledDev.Bounds().Dy(), g.Image[i])
+	}
+
+	// Display the frames in a loop:
+	for i := 0; ; i++ {
+		index := i % len(imgs)
+		c := time.After(time.Duration(10*g.Delay[index]) * time.Millisecond)
+		img := imgs[index]
+		oledDev.Draw(img.Bounds(), img, image.Point{})
+		<-c
+	}
+
 	/*
-		// Decodes an animated GIF as specified on the command line:
-		if len(os.Args) != 2 {
-			log.Fatal("please provide the path to an animated GIF")
+		img := image1bit.NewVerticalLSB(oledDev.Bounds())
+		f := basicfont.Face7x13
+		drawer := font.Drawer{
+			Dst:  img,
+			Src:  &image.Uniform{image1bit.On},
+			Face: f,
+			Dot:  fixed.P(0, img.Bounds().Dy()-1-f.Descent),
 		}
-		f, err := os.Open(os.Args[1])
+		drawer.DrawString("Hello World!")
+		err = oledDev.Draw(img.Bounds(), img, image.Point{})
 		if err != nil {
-			log.Fatal(err)
-		}
-		g, err := gif.DecodeAll(f)
-		f.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Converts every frame to image.Gray and resize them:
-		imgs := make([]*image.Gray, len(g.Image))
-		for i := range g.Image {
-			imgs[i] = convertAndResizeAndCenter(oledDev.Bounds().Dx(), oledDev.Bounds().Dy(), g.Image[i])
-		}
-
-		// Display the frames in a loop:
-		for i := 0; ; i++ {
-			index := i % len(imgs)
-			c := time.After(time.Duration(10*g.Delay[index]) * time.Millisecond)
-			img := imgs[index]
-			oledDev.Draw(img.Bounds(), img, image.Point{})
-			<-c
+			panic(err)
 		}
 	*/
-	img := image1bit.NewVerticalLSB(oledDev.Bounds())
-	f := basicfont.Face7x13
-	drawer := font.Drawer{
-		Dst:  img,
-		Src:  &image.Uniform{image1bit.On},
-		Face: f,
-		Dot:  fixed.P(0, img.Bounds().Dy()-1-f.Descent),
-	}
-	drawer.DrawString("Hello World!")
-	err = oledDev.Draw(img.Bounds(), img, image.Point{})
-	if err != nil {
-		panic(err)
-	}
 }
